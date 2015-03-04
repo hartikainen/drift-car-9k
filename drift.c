f1c430a73f2093b49de1c9aad13666d6de24af14
Risto Vuorio <risto@apportable.com>
2015-03-04 14:30:40 +0200

    naive test for setting up motor pwm

--- drift.c
+++ drift.c
@@ -2,21 +2,28 @@
 #include <avr/io.h>   /* definitions for all PORT* and other registers. You absolutely will need this one */
 #include <avr/interrupt.h>
 
-// TODO: remove this
-#include <util/delay.h>
+#include <delay.h>
 
-#define LEDS_PORT PORTC
-#define LEDS_DDR  DDRC
-#define LEDS_PIN  PINC
 
-#define SWITCH_PORT PORTA
-#define SWITCH_DDR DDRA
-#define SWITCH_PIN PINA
+void setup_pwm(int pwmoffset)
+{
+    DDRH |= 1 << PH3; // PH3 OC4A (Output Compare and PWM Output A for Timer/Counter4)
+    TCCR4A |= 1 << WGM41 | 1 << COM4A1 | 1 << COM4A0;
+    TCCR4B |= 1 << WGM43 | 1 << WGM42 | 1 << CS40 | 1 << CS41;
+    ICR1 = 4999;
+    OCR1A = ICR1 - pwmoffset;
+}
 
-#define BLINK_LED PC2
-#define OTHER_LED PC0
+void disable_pwm(void)
+{
+    TCCR1A &= ~_BV(COM1A1);
+}
+
 
 int main(void) {
-  for(;;) {
-  }
+    setup_pwm(500);
+    _delay_ms(500);
+    disable_pwm;
+    for(;;) {
+    }
 }
