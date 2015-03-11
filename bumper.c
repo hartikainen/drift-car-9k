4692b5830fd0262a9083d5c099863a4979b59def
Hartikainen Kristian <hartikk1@breakdown.niksula.hut.fi>
2015-03-11 17:11:16 +0000

    Removed the moving average stuff
    This reverts commit 98e274644415b16c46472dd82462584cf1a90623.

--- bumper.c
+++ bumper.c
@@ -59,95 +59,11 @@ void reset_PID_stuff(void) {
 
   /* output_string(kistr,4,4); */
 }
-
-int round_int(float r)
-{
-  // http://stackoverflow.com/a/4660122
-  return (r > 0.0) ? (r + 0.5) : (r - 0.5);
-}
-
-int log2(int x)
-{
-  // http://stackoverflow.com/a/9612244
-  int ans = 0 ;
-  while(x>>=1)
-  {
-    ans++;
-  }
-  return ans;
-}
-
-int ipow(int exp)
-{
-  // http://stackoverflow.com/a/101613
-  int base = 2;
-  int result = 1;
-  while (exp)
-  {
-    if (exp & 1)
-    {
-      result *= base;
-    }
-    exp >>= 1;
-    base *= base;
-  }
-  return result;
-}
-
-int moving_avg( int x )
-{
-  // doesn't handle bits like 0b001100000 correctly
-  static float old[] = {0.0,0.0,0.0};
-  float y;
-  /* if (x == 0) */
-  /* { */
-  /*   // We sampled 0b00000000 but the last sample was not 0b10000000, 0b00000001 or 0b00000000 pad with last sample. */
-  /*   if (old[2] > 1.0 && old[2] < 7.0) */
-  /*   { */
-  /*     y = old[2]; */
-  /*   } */
-  /*   else */
-  /*   { */
-  /*     y = 0.0; */
-  /*   } */
-  /* } */
-
-  if (x == 0)
-  {
-    y = x;
-  }
-  else
-  {
-    // calculate log2 of sample because averaging won't work well over values like 128, 64, 32...
-    if (x > 1)
-    {
-      y = log2(x);
-    }
-    else
-    {
-      y = x;
-    }
-  }
-
-  // calculate moving average over 4 measurements
-  float new_value = (old[0]+old[1]+old[2]+y)/4;
-  old[0] = old[1];
-  old[1] = old[2];
-  old[2] = y;
-
-  // calculate the power of 2 to return value in the correct form again
-  if (new_value != 0.0)
-  {
-    return ipow(round_int(new_value));
-  }
-  return 0;
-}
-
 void read_bumper_turn_wheels(void)
 {
   float y;
   uint8_t bp = ~BUMPER_PIN;
-  bumper = moving_avg(bp);
+  bumper = bp;
   switch (bumper) {
   case 0b00000001:
     bumper_int = -4;
